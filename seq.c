a0dae399d666f7aba159ed275bc7f2f9f5067d72
Fuga Kato <tresener.K@gmail.com>
2017-07-29 22:59:52 +0900

    miscdevice

--- seq.c
+++ seq.c
@@ -1,18 +1,58 @@
-#include <linux/init.h>
 #include <linux/module.h>
-MODULE_LICENSE("GPL");
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
 
-static int seq_init(void)
+static ssize_t seq_read(struct file *filp, char __user *buf,
+			size_t count, loff_t *ppos)
 {
-	printk(KERN_ALERT "seq: init\n");
 	return 0;
 }
 
-static void seq_exit(void)
+static ssize_t seq_write(struct file *filp, const char __user *buf,
+			 size_t count, loff_t *ppos)
 {
-	printk(KERN_ALERT "seq: exit\n");
+	return count;
 }
 
+static long seq_ioctl(struct file *filp, unsigned int cmd,
+		     unsigned long arg)
+{
+	return  -ENOTTY;
+}
+
+static struct file_operations seq_fops = {
+	.owner		= THIS_MODULE,
+	.read		= seq_read,
+	.write		= seq_write,
+	.unlocked_ioctl	= seq_ioctl,
+};
+
+static struct miscdevice seq_dev = {
+	.minor	= MISC_DYNAMIC_MINOR,
+	.name	= "seq",
+	.fops	= &seq_fops
+};
+
+static int __init seq_init(void)
+{
+	int r;
+
+	if ((r = misc_register(&seq_dev))) {
+		printk("seq: misc_register failed\n");
+		return r;
+	}
+
+	printk("seq: loaded\n");
+	return 0;
+}
+
+static void __exit seq_exit(void)
+{
+	printk(KERN_INFO "seq: exit\n");
+	misc_deregister(&seq_dev);
+}
+
+MODULE_LICENSE("GPL");
 module_init(seq_init);
 module_exit(seq_exit);
 
